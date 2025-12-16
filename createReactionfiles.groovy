c166e5129bbb1d8bea826fc8b4cea4d3d1c72470
Egon Willighagen <egon.willighagen@gmail.com>
2025-12-16 07:38:46 +0100

    Create RDF for both pathways and reactions

---
+++ createReactionfiles.groovy
@@ -0,0 +1,29 @@
+import java.util.ArrayList
+import java.util.List
+import java.util.Map
+import java.util.HashMap
+
+import java.nio.file.Path
+import java.nio.file.Files
+
+import static groovy.io.FileType.FILES
+
+Map<String,List> failedTests = new HashMap<>();
+
+def dir = new File("orig");
+def files = [];
+dir.traverse(type: FILES, maxDepth: 0) {
+  if (it.name.endsWith(".gpml")) {
+    files.add(it)
+  }
+};
+
+counter = 0
+files.each { file ->
+  counter = counter + 1
+  wpid = "RC" + counter
+  println file.name + " -> ${wpid}"
+  source = new File(file.path)
+  target = new File("orig-renamed/${wpid}.gpml")
+  Files.copy(source.toPath(), target.toPath())
+}
